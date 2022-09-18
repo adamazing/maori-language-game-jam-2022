4875bff7b662da8a42921d47261a0cdb24b657bd
Adam Henley <adamazing@gmail.com>
2022-09-18 20:13:04 +1200

    Kiwi Animated
    Signed-off-by: Adam Henley <adamazing@gmail.com>

--- src/kiwi.rs
+++ src/kiwi.rs
@@ -19,6 +19,7 @@ impl Plugin for KiwiPlugin {
                 ConditionSet::new()
                     .run_in_state(GameState::GamePlaying)
                     .run_not_in_state(PauseState::Paused)
+                    .with_system(kiwi_peck_tracker)
                     .with_system(movement)
                     .with_system(animate_kiwi)
                     .into(),
@@ -39,6 +40,8 @@ pub struct Kiwi;
 #[derive(Bundle, Default, LdtkEntity)]
 struct KiwiBundle {
     kiwi: Kiwi,
+
+    kiwi_peck_state: KiwiPeckState,
 
     #[bundle]
     #[sprite_sheet_bundle]
@@ -134,6 +137,23 @@ impl From<IntGridCell> for ColliderBundle {
     }
 }
 
+struct PeckStateTimer(Timer);
+
+impl Default for PeckStateTimer {
+    fn default() -> Self {
+        Self(Timer::from_seconds(1.5, true))
+    }
+}
+
+#[derive(Component, Default, Debug,PartialEq)]
+enum KiwiPeckState {
+    Pecking,
+
+    #[default]
+    Idle,
+}
+
+
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
@@ -148,6 +168,7 @@ fn animate_kiwi(
         (
             &mut Velocity,
             &mut TextureAtlasSprite,
+            &mut KiwiPeckState,
         ),
         With<Kiwi>
     >,
@@ -155,49 +176,86 @@ fn animate_kiwi(
     time: Res<Time>
 ) {
 
-    for (mut velocity, mut sprite) in kiwi_query.iter_mut() {
-        sprite.index = if velocity.linear.x > 0. {
-            // info!("animate right");
-           sprite.flip_x = false;
-           animation_timer.0.tick(time.delta());
-           let index = 6 + ((sprite.index + if animation_timer.0.finished(){
-               animation_timer.0.reset();
-               1
-           } else {
-               0
-           }) % 4);
-           info!(" Sprite index: {}", index);
-           index
-        } else if velocity.linear.x < 0. {
-           // info!("animate left");
-           sprite.flip_x = true;
-           animation_timer.0.tick(time.delta());
-           let index = 6 + ((sprite.index + if animation_timer.0.finished(){
-               animation_timer.0.reset();
-               1
-           } else {
-               0
-           }) % 4);
-           info!(" Sprite index: {}", index);
-           index
+    for (mut velocity, mut sprite, mut peck_state) in kiwi_query.iter_mut() {
+        if *peck_state == KiwiPeckState::Pecking {
+            animation_timer.0.tick(time.delta());
+            sprite.index = 3 + ((sprite.index + if animation_timer.0.finished() {
+                animation_timer.0.reset();
+                1
+            } else {
+                0
+            }) % 3);
         } else {
-            // sprite.index = 4;
-            // info!("animate idle if time has passed");
-            6
+            sprite.index = if velocity.linear.x > 0. {
+                // info!("animate right");
+               sprite.flip_x = false;
+               animation_timer.0.tick(time.delta());
+               let index = 6 + ((sprite.index + if animation_timer.0.finished(){
+                   animation_timer.0.reset();
+                   1
+               } else {
+                   0
+               }) % 4);
+               // info!(" Sprite index: {}", index);
+               index
+            } else if velocity.linear.x < 0. {
+               // info!("animate left");
+               sprite.flip_x = true;
+               animation_timer.0.tick(time.delta());
+               let index = 6 + ((sprite.index + if animation_timer.0.finished(){
+                   animation_timer.0.reset();
+                   1
+               } else {
+                   0
+               }) % 4);
+               // info!(" Sprite index: {}", index);
+               index
+            } else {
+                0
+                // info!("animate idle if time has passed");
+                // let index = 2 + ((sprite.index + if animation_timer.0.finished(){
+                //     animation_timer.0.reset();
+                //     1
+                // }else{
+                //     0
+                // }) % 2);
+                // info!(" Sprite index: {}", index);
+                // index
+            }
         }
         // info!("{}", velocity.linear.x);
     }
 }
 
+fn kiwi_peck_tracker(
+    mut kiwi_query: Query<(&mut KiwiPeckState, &mut TextureAtlasSprite, &Transform), With<Kiwi>>,
+    mut timer: Local<PeckStateTimer>,
+    time: Res<Time>,
+    ){
+    timer.0.tick(time.delta());
+
+    if timer.0.finished(){
+
+        info!("PeckStateTimer finished");
+        for (mut kiwi_peck_state, mut sprite, transform) in kiwi_query.iter_mut() {
+            if *kiwi_peck_state == KiwiPeckState::Pecking {
+                info!("Reset to idle peckstate");
+                *kiwi_peck_state = KiwiPeckState::Idle;
+                sprite.index = 0;
+            }
+        }
+    }
+
+}
 
 fn movement(
     mut query: Query<
-        (&mut Velocity, &mut ActionState<KiwiAction>),
+        (&mut Velocity, &mut ActionState<KiwiAction>, &mut KiwiPeckState),
         (With<Kiwi>, Changed<ActionState<KiwiAction>>),
     >
 ) {
     // debug!("Movement");
-    for (mut velocity, action_state) in query.iter_mut() {
+    for (mut velocity, mut action_state, mut peck_state) in query.iter_mut() {
         // debug!("In query loop");
         let right = if action_state.pressed(KiwiAction::Right) {
             1.
@@ -210,19 +268,14 @@ fn movement(
             0.
         };
 
-        velocity.linear.x = (right - left) * 100.;
-
-        if velocity.linear.x > 0. {
-            debug!("heading right?");
-        } else if velocity.linear.x < 0. {
-            debug!("heading left?");
-        } else {
-            debug!("stationary");
+        if *peck_state != KiwiPeckState::Pecking {
+            velocity.linear.x = (right - left) * 100.;
         }
-        // info!("{} {}", left, right);
 
         if action_state.pressed(KiwiAction::Peck) {
             info!("Pecking");
+            *peck_state = KiwiPeckState::Pecking;
+            velocity.linear.x = 0.;
             // velocity.linear.y = 250.;
             // climber.climbing = false;
         }
